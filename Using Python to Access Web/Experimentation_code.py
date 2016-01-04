483e6c74cb42014bf36739528774ead336187554
unknown <wcai@WCAIWL1390011.njgroup.com>
2016-01-03 21:34:34 -0500

    1/3/16

--- Using Python to Access Web/Experimentation_code.py
+++ Using Python to Access Web/Experimentation_code.py
@@ -1,50 +1,11 @@
 """
-Server Side
-Sending and Recieving Data with Sockets
+Webscraper
 """
-import socket
-import sys
-from thread import * #Python 2.7
-#from _thread import * #Python 3.2
+#import urllib #python 2.7
+import urllib.request #python 3.5
 
-host = ""
-port = 7777
+#headjack = urllib.urlopen("http://www.dr-chuck.com/page1.htm") #python 2.7
+headjack = urllib.request.urlopen("http://www.dr-chuck.com/page1.htm") #python 3.5
 
-headjack = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    headjack.bind((host, port))
-except socket.error as error:
-    print(str(error))
-
-headjack.listen(1)
-print("Waiting for a connection...")
-
-def threaded_client(connection):
-    connection.send(str("Type here:\n")) #Python 2.7
-    #connection.send(str.encode("Type here:\n")) #Python 3.2
-
-    while True:
-        data = connection.recv(1024) #originally 2048
-        print("Received ", repr(data)) #Python 2.7
-        #print("Received ", repr(data.decode("utf-8"))) #Python 3.2
-        ###reply = "Server output: " + data.decode("utf-8") #original test
-        reply = raw_input("Reply :") #Python 2.7
-        #reply = input("Reply :") #Python 3.2
-        if not data:
-            break
-        connection.sendall(str(reply)) #Python 2.7
-        #connection.sendall(str.encode(reply)) #Python 3.2
-
-    connection.close()
-
-while True:
-    connection, address = headjack.accept()
-    print("connected to: "+address[0]+":"+str(address[1]))
-    start_new_thread(threaded_client,(connection,))
-
-#while True:
-    #data = conn.recv(1024) #recives datae (1024 bytes) using conn and store into data
-    #print("Received ", repr(data)) # print data; Data is the message the users types
-    #reply = raw_input("Reply: ")
-    #conn.sendall(reply)
+for line in headjack:
+    print(line.strip())
